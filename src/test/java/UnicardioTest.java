1b0b85b664432c4882b910ec1dbe337152fbbe8a
Paulodev88 <paulo.ricardodev88@gmail.com>
2021-06-13 17:45:16 -0300

    Added the test - must register the patient

--- src/test/java/UnicardioTest.java
+++ src/test/java/UnicardioTest.java
@@ -32,55 +32,113 @@ public class UnicardioTest {
 			return driver;
 		}	
 
-	@Test
-	public void devoAcessarHomePage() {		
-		
-		
-					
-		acessarAplicação().quit();		
-	}	
+//	@Test
+//	public void devoAcessarHomePage() {		
+//		
+//		
+//					
+//		acessarAplicação().quit();		
+//	}	
+//	
+//	@Test
+//	public void devoLogarComoPaciente() {		
+//		WebDriver driver = acessarAplicação();
+//		
+//		try {
+//		//Clickar em login
+//		driver.findElement(By.id("btn-login")).click();		
+//		//Escrever CPF
+//		driver.findElement(By.id("input-cpf")).sendKeys("192");
+//		//Escrever Senha
+//		driver.findElement(By.id("input-senha")).sendKeys("123456");
+//		//Clickar em Entrar
+//		driver.findElement(By.className("btn-login")).click();	
+//		}
+//		finally {
+//			//Fechar o drive			
+//			driver.quit();
+//		}
+//	}
+//	
+//	@Test
+//	public void devoLogarComoAdm() {		
+//		WebDriver driver = acessarAplicação();
+//		try {
+//		//Clickar em login
+//		driver.findElement(By.id("btn-login")).click();		
+//		//Escrever CPF
+//		driver.findElement(By.id("input-cpf")).sendKeys("123456");
+//		//Escrever Senha
+//		driver.findElement(By.id("input-senha")).sendKeys("123456");
+//		//Clickar em Entrar
+//		driver.findElement(By.className("btn-login")).click();
+//		}
+//		finally {
+//			//Fechar o drive			
+//			driver.quit();
+//		}
+//	}	
+//	
+//	@Test
+//	public void devoRegistrarMedico() {		
+//		WebDriver driver = acessarAplicação();
+//		
+//		try {
+//		
+//		//Clickar em login
+//		driver.findElement(By.id("btn-login")).click();		
+//		//Escrever CPF
+//		driver.findElement(By.id("input-cpf")).sendKeys("123456");
+//		//Escrever Senha
+//		driver.findElement(By.id("input-senha")).sendKeys("123456");
+//		//Clickar em Entrar
+//		driver.findElement(By.className("btn-login")).click();	
+//		
+//		
+//		//Abrir uma Nova Aba
+//		try {
+//            Robot robot = new Robot();
+//            robot.keyPress(KeyEvent.VK_CONTROL);
+//            robot.keyPress(KeyEvent.VK_T);
+//            robot.keyRelease(KeyEvent.VK_CONTROL);
+//            robot.keyRelease(KeyEvent.VK_T);
+//			} catch (AWTException ex) {
+//				throw new WebDriverException("Erro ao digitar CTRL + T", ex);
+//			}
+//
+//        WebDriverWait wait = new WebDriverWait(driver, 5);
+//        wait.until(ExpectedConditions.numberOfWindowsToBe(2));
+//
+//        List<String> windowHandles = new ArrayList<>(driver.getWindowHandles());
+//        driver.switchTo().window(windowHandles.get(1));
+//        
+//        //Acessar o registro medico com ADM
+//        driver.get("http://localhost:8080/registros/medico");				
+//		
+//		//Escrever Nome
+//		driver.findElement(By.id("input-nome")).sendKeys("Medico Selenium");		
+//		//Digitar CPF
+//		driver.findElement(By.id("input-cpf")).sendKeys(""+ gerador.nextInt());		
+//		//Digitar CRM
+//		driver.findElement(By.id("input-crm")).sendKeys(""+gerador.nextInt());			
+//		//Selecionar Tipo Medico
+//		WebElement selectElement = driver.findElement(By.id("input-tipo"));
+//		Select selectObject = new Select(selectElement);
+//		selectObject.selectByValue("profissional");		
+//		//Digitar Senha
+//		driver.findElement(By.id("input-senha")).sendKeys("123456");	
+//		//Confirmar a Senha
+//		driver.findElement(By.id("input-confirma-senha")).sendKeys("123456");
+//		//Clickar em Registrar	
+//		}
+//		finally {
+//			//Fechar o drive			
+//			driver.quit();
+//		}
+//	}
 	
 	@Test
-	public void devoLogarComoPaciente() {		
-		WebDriver driver = acessarAplicação();
-		
-		try {
-		//Clickar em login
-		driver.findElement(By.id("btn-login")).click();		
-		//Escrever CPF
-		driver.findElement(By.id("input-cpf")).sendKeys("192");
-		//Escrever Senha
-		driver.findElement(By.id("input-senha")).sendKeys("123456");
-		//Clickar em Entrar
-		driver.findElement(By.className("btn-login")).click();	
-		}
-		finally {
-			//Fechar o drive			
-			driver.quit();
-		}
-	}
-	
-	@Test
-	public void devoLogarComoAdm() {		
-		WebDriver driver = acessarAplicação();
-		try {
-		//Clickar em login
-		driver.findElement(By.id("btn-login")).click();		
-		//Escrever CPF
-		driver.findElement(By.id("input-cpf")).sendKeys("123456");
-		//Escrever Senha
-		driver.findElement(By.id("input-senha")).sendKeys("123456");
-		//Clickar em Entrar
-		driver.findElement(By.className("btn-login")).click();
-		}
-		finally {
-			//Fechar o drive			
-			driver.quit();
-		}
-	}	
-	
-	@Test
-	public void devoRegistrarMedico() {		
+	public void devoRegistrarPaciente() {		
 		WebDriver driver = acessarAplicação();
 		
 		try {
@@ -113,23 +171,24 @@ public class UnicardioTest {
         driver.switchTo().window(windowHandles.get(1));
         
         //Acessar o registro medico com ADM
-        driver.get("http://localhost:8080/registros/medico");				
+        driver.get("http://localhost:8080/registros/paciente");				
 		
 		//Escrever Nome
 		driver.findElement(By.id("input-nome")).sendKeys("Medico Selenium");		
 		//Digitar CPF
 		driver.findElement(By.id("input-cpf")).sendKeys(""+ gerador.nextInt());		
-		//Digitar CRM
-		driver.findElement(By.id("input-crm")).sendKeys(""+gerador.nextInt());			
-		//Selecionar Tipo Medico
-		WebElement selectElement = driver.findElement(By.id("input-tipo"));
-		Select selectObject = new Select(selectElement);
-		selectObject.selectByValue("profissional");		
+		//Digitar Cor
+		driver.findElement(By.id("input-cor")).sendKeys("Verde");			
+		//Digitar Sexo
+		driver.findElement(By.id("input-sexo")).sendKeys("Alguma coisa");	
+		//Digitar DataNascimento
+		driver.findElement(By.id("input-data-nascimento")).sendKeys("07091988");	
 		//Digitar Senha
-		driver.findElement(By.id("input-senha")).sendKeys("123456");	
+		driver.findElement(By.id("input-senha")).sendKeys("123456");
 		//Confirmar a Senha
 		driver.findElement(By.id("input-confirma-senha")).sendKeys("123456");
 		//Clickar em Registrar	
+		
 		}
 		finally {
 			//Fechar o drive			
