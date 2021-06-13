d22b3e91d13efd2ad23b55f122ccb5ae4e881943
Paulodev88 <paulo.ricardodev88@gmail.com>
2021-06-13 18:22:28 -0300

    Added the test - must register the exam

--- src/test/java/UnicardioTest.java
+++ src/test/java/UnicardioTest.java
@@ -32,110 +32,110 @@ public class UnicardioTest {
 			return driver;
 		}	
 
-//	@Test
-//	public void devoAcessarHomePage() {		
-//		
-//		
-//					
-//		acessarAplicação().quit();		
-//	}	
-//	
-//	@Test
-//	public void devoLogarComoPaciente() {		
-//		WebDriver driver = acessarAplicação();
-//		
-//		try {
-//		//Clickar em login
-//		driver.findElement(By.id("btn-login")).click();		
-//		//Escrever CPF
-//		driver.findElement(By.id("input-cpf")).sendKeys("192");
-//		//Escrever Senha
-//		driver.findElement(By.id("input-senha")).sendKeys("123456");
-//		//Clickar em Entrar
-//		driver.findElement(By.className("btn-login")).click();	
-//		}
-//		finally {
-//			//Fechar o drive			
-//			driver.quit();
-//		}
-//	}
-//	
-//	@Test
-//	public void devoLogarComoAdm() {		
-//		WebDriver driver = acessarAplicação();
-//		try {
-//		//Clickar em login
-//		driver.findElement(By.id("btn-login")).click();		
-//		//Escrever CPF
-//		driver.findElement(By.id("input-cpf")).sendKeys("123456");
-//		//Escrever Senha
-//		driver.findElement(By.id("input-senha")).sendKeys("123456");
-//		//Clickar em Entrar
-//		driver.findElement(By.className("btn-login")).click();
-//		}
-//		finally {
-//			//Fechar o drive			
-//			driver.quit();
-//		}
-//	}	
-//	
-//	@Test
-//	public void devoRegistrarMedico() {		
-//		WebDriver driver = acessarAplicação();
-//		
-//		try {
-//		
-//		//Clickar em login
-//		driver.findElement(By.id("btn-login")).click();		
-//		//Escrever CPF
-//		driver.findElement(By.id("input-cpf")).sendKeys("123456");
-//		//Escrever Senha
-//		driver.findElement(By.id("input-senha")).sendKeys("123456");
-//		//Clickar em Entrar
-//		driver.findElement(By.className("btn-login")).click();	
-//		
-//		
-//		//Abrir uma Nova Aba
-//		try {
-//            Robot robot = new Robot();
-//            robot.keyPress(KeyEvent.VK_CONTROL);
-//            robot.keyPress(KeyEvent.VK_T);
-//            robot.keyRelease(KeyEvent.VK_CONTROL);
-//            robot.keyRelease(KeyEvent.VK_T);
-//			} catch (AWTException ex) {
-//				throw new WebDriverException("Erro ao digitar CTRL + T", ex);
-//			}
-//
-//        WebDriverWait wait = new WebDriverWait(driver, 5);
-//        wait.until(ExpectedConditions.numberOfWindowsToBe(2));
-//
-//        List<String> windowHandles = new ArrayList<>(driver.getWindowHandles());
-//        driver.switchTo().window(windowHandles.get(1));
-//        
-//        //Acessar o registro medico com ADM
-//        driver.get("http://localhost:8080/registros/medico");				
-//		
-//		//Escrever Nome
-//		driver.findElement(By.id("input-nome")).sendKeys("Medico Selenium");		
-//		//Digitar CPF
-//		driver.findElement(By.id("input-cpf")).sendKeys(""+ gerador.nextInt());		
-//		//Digitar CRM
-//		driver.findElement(By.id("input-crm")).sendKeys(""+gerador.nextInt());			
-//		//Selecionar Tipo Medico
-//		WebElement selectElement = driver.findElement(By.id("input-tipo"));
-//		Select selectObject = new Select(selectElement);
-//		selectObject.selectByValue("profissional");		
-//		//Digitar Senha
-//		driver.findElement(By.id("input-senha")).sendKeys("123456");	
-//		//Confirmar a Senha
-//		driver.findElement(By.id("input-confirma-senha")).sendKeys("123456");
-//		//Clickar em Registrar	
-//		}
-//		finally {
-//			//Fechar o drive			
-//			driver.quit();
-//		}
-//	}
+	@Test
+	public void devoAcessarHomePage() {		
+		
+		
+					
+		acessarAplicação().quit();		
+	}	
+	
+	@Test
+	public void devoLogarComoPaciente() {		
+		WebDriver driver = acessarAplicação();
+		
+		try {
+		//Clickar em login
+		driver.findElement(By.id("btn-login")).click();		
+		//Escrever CPF
+		driver.findElement(By.id("input-cpf")).sendKeys("192");
+		//Escrever Senha
+		driver.findElement(By.id("input-senha")).sendKeys("123456");
+		//Clickar em Entrar
+		driver.findElement(By.className("btn-login")).click();	
+		}
+		finally {
+			//Fechar o drive			
+			driver.quit();
+		}
+	}
+	
+	@Test
+	public void devoLogarComoAdm() {		
+		WebDriver driver = acessarAplicação();
+		try {
+		//Clickar em login
+		driver.findElement(By.id("btn-login")).click();		
+		//Escrever CPF
+		driver.findElement(By.id("input-cpf")).sendKeys("123456");
+		//Escrever Senha
+		driver.findElement(By.id("input-senha")).sendKeys("123456");
+		//Clickar em Entrar
+		driver.findElement(By.className("btn-login")).click();
+		}
+		finally {
+			//Fechar o drive			
+			driver.quit();
+		}
+	}	
+	
+	@Test
+	public void devoRegistrarMedico() {		
+		WebDriver driver = acessarAplicação();
+		
+		try {
+		
+		//Clickar em login
+		driver.findElement(By.id("btn-login")).click();		
+		//Escrever CPF
+		driver.findElement(By.id("input-cpf")).sendKeys("123456");
+		//Escrever Senha
+		driver.findElement(By.id("input-senha")).sendKeys("123456");
+		//Clickar em Entrar
+		driver.findElement(By.className("btn-login")).click();	
+		
+		
+		//Abrir uma Nova Aba
+		try {
+            Robot robot = new Robot();
+            robot.keyPress(KeyEvent.VK_CONTROL);
+            robot.keyPress(KeyEvent.VK_T);
+            robot.keyRelease(KeyEvent.VK_CONTROL);
+            robot.keyRelease(KeyEvent.VK_T);
+			} catch (AWTException ex) {
+				throw new WebDriverException("Erro ao digitar CTRL + T", ex);
+			}
+
+        WebDriverWait wait = new WebDriverWait(driver, 5);
+        wait.until(ExpectedConditions.numberOfWindowsToBe(2));
+
+        List<String> windowHandles = new ArrayList<>(driver.getWindowHandles());
+        driver.switchTo().window(windowHandles.get(1));
+        
+        //Acessar o registro medico com ADM
+        driver.get("http://localhost:8080/registros/medico");				
+		
+		//Escrever Nome
+		driver.findElement(By.id("input-nome")).sendKeys("Medico Selenium");		
+		//Digitar CPF
+		driver.findElement(By.id("input-cpf")).sendKeys(""+ gerador.nextInt());		
+		//Digitar CRM
+		driver.findElement(By.id("input-crm")).sendKeys(""+gerador.nextInt());			
+		//Selecionar Tipo Medico
+		WebElement selectElement = driver.findElement(By.id("input-tipo"));
+		Select selectObject = new Select(selectElement);
+		selectObject.selectByValue("profissional");		
+		//Digitar Senha
+		driver.findElement(By.id("input-senha")).sendKeys("123456");	
+		//Confirmar a Senha
+		driver.findElement(By.id("input-confirma-senha")).sendKeys("123456");
+		//Clickar em Registrar	
+		}
+		finally {
+			//Fechar o drive			
+			driver.quit();
+		}
+	}
 	
 	@Test
 	public void devoRegistrarPaciente() {		
@@ -170,11 +170,11 @@ public class UnicardioTest {
         List<String> windowHandles = new ArrayList<>(driver.getWindowHandles());
         driver.switchTo().window(windowHandles.get(1));
         
-        //Acessar o registro medico com ADM
+        //Acessar o registro paciente com ADM
         driver.get("http://localhost:8080/registros/paciente");				
 		
 		//Escrever Nome
-		driver.findElement(By.id("input-nome")).sendKeys("Medico Selenium");		
+		driver.findElement(By.id("input-nome")).sendKeys("Paciente Selenium");		
 		//Digitar CPF
 		driver.findElement(By.id("input-cpf")).sendKeys(""+ gerador.nextInt());		
 		//Digitar Cor
@@ -195,4 +195,70 @@ public class UnicardioTest {
 			driver.quit();
 		}
 	}
+	
+	@Test
+	public void devoRegistrarExame() {		
+		WebDriver driver = acessarAplicação();
+		
+		try {
+		
+		//Clickar em login para logar como Professor
+		driver.findElement(By.id("btn-login")).click();		
+		//Escrever CPF
+		driver.findElement(By.id("input-cpf")).sendKeys("930");
+		//Escrever Senha
+		driver.findElement(By.id("input-senha")).sendKeys("123456");
+		//Clickar em Entrar
+		driver.findElement(By.className("btn-login")).click();	
+		
+		
+		//Abrir uma Nova Aba
+		try {
+            Robot robot = new Robot();
+            robot.keyPress(KeyEvent.VK_CONTROL);
+            robot.keyPress(KeyEvent.VK_T);
+            robot.keyRelease(KeyEvent.VK_CONTROL);
+            robot.keyRelease(KeyEvent.VK_T);
+			} catch (AWTException ex) {
+				throw new WebDriverException("Erro ao digitar CTRL + T", ex);
+			}
+
+        WebDriverWait wait = new WebDriverWait(driver, 5);
+        wait.until(ExpectedConditions.numberOfWindowsToBe(2));
+
+        List<String> windowHandles = new ArrayList<>(driver.getWindowHandles());
+        driver.switchTo().window(windowHandles.get(1));
+        
+        //Acessar o registro exame como Medico
+        driver.get("http://localhost:8080/registros/exame");				
+		
+			
+		//Digitar CPF
+		driver.findElement(By.id("input-cpf")).sendKeys("192");
+		//Selecionar CID
+		WebElement selectElement = driver.findElement(By.id("input-cid"));
+		Select selectObject = new Select(selectElement);
+		selectObject.selectByValue("A00");			
+		//Digitar Data
+		driver.findElement(By.id("input-data-exame")).sendKeys("13062021");	
+		//Selecionar Tipo Solicitante
+		WebElement selectElement1 = driver.findElement(By.id("input-tipo-solicitante"));
+		Select selectObject1 = new Select(selectElement1);
+		selectObject1.selectByValue("professor");
+		//Selecionar Tipo Exame
+		WebElement selectElement2 = driver.findElement(By.id("input-tipo-exame"));
+		Select selectObject2 = new Select(selectElement2);
+		selectObject2.selectByValue("2");	
+		//Digitar Recomendação
+		driver.findElement(By.id("input-recomendacoes")).sendKeys("Esse teste é recomendado por Paulo Ricardo");
+		//Clickar em Registrar	
+		
+		}
+		finally {
+			//Fechar o drive			
+			driver.quit();
+		}
+	}
+	
+	
 }
